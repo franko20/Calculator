f16f47c4c4c06bddc156b65e5500ac39e9ffd7cf
franko20 <84520567+franko20@users.noreply.github.com>
2021-05-21 00:50:33 +0530

    Update calc.js

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -2,29 +2,23 @@
 
 function dis(val)
 {
-    document.getElementById("input").value += val
+    document.getElementById("input").value += val;
 
-    let x = document.getElementById("input").value
-    let y = eval(x);
-    let z = y
-    document.getElementById("result").value = ""
-    document.getElementById("result").value = z
+    let x = document.getElementById("input").value;
+    document.getElementById("result").value = eval(x);
 }
 
 //display the answer as input
 
 function solve()
 {
-    let x = document.getElementById("input").value
-    let y = eval(x);
-    let z = y
-    document.getElementById("input").value = ""
-    document.getElementById("input").value = z
+    let x = document.getElementById("input").value;
+    document.getElementById("input").value = eval(x);
 }
  //clear the display
 
  function clr()
  {
-    document.getElementById("input").value = ""
-    document.getElementById("result").value = ""
+    document.getElementById("input").value = "";
+    document.getElementById("result").value = "";
  }
